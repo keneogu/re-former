ed9c7d3d1b5c34de43a3b1cbe79b012b0a07a33e
warblo001 <agealan_01@hotmail.com>
2021-02-18 11:38:02 +0100

    Add editing to controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,12 @@ class UsersController < ApplicationController
 	  end
 	end
 
+	def edit
+    @user = User.find_by(id:params[:id])
+  end
+
 	private
-      def user_params
-		params.require(:user).permit(:username,:email,:password,:authenticity_token)
-	  end
+  def user_params
+		params.require(:user).permit(:username,:email,:password)
+	end
 end
